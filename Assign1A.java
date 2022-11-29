91ac94c228638af07a9452ce3e8c96648990f6ec
Divyanshu shekhar <103349337+shekhar3742@users.noreply.github.com>
2022-11-30 00:12:23 +0530

    Update Assign1A.java

--- Assign1A.java
+++ Assign1A.java
@@ -1,5 +1,5 @@
 public class Assign1A{
     public static void main(String [] args) {
-        System.out.println("This is Nalla Shekhar.");
+        System.out.println("This is Shekhar.");
     }
 }
